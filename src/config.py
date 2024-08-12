c0bc573e80cb5e1932fb4845b110ac6977da52df
EvilGiraffes <thomas.e.brekke@hotmail.no>
2024-08-12 20:00:07 +0200

    Moved config

--- src/config.py
+++ src/config.py
@@ -4,6 +4,40 @@ import log
 
 from argparse import ArgumentParser, Namespace
 
+
+def setup(parser: ArgumentParser, default_log_level: log.Level, strategies: Iterable[str]) -> "_Config":
+    assert default_log_level > 9, "Default level below nine will cause unforseen issues"
+    assert (default_log_level / 10).is_integer(), "Default level must be divisable by 10, use the logging constants"
+
+    parser.add_argument(
+        "src",
+        type=Path,
+        help="The source folder to copy from",
+    )
+    parser.add_argument(
+        "dst",
+        type=Path,
+        help="The destination folder to copy to",
+    )
+    parser.add_argument(
+        "-s", "--strategy",
+        choices=list(strategies),
+        help="The strategy to use when doing a backup"
+    )
+    parser.add_argument(
+        "-v",
+        default=0,
+        action="count",
+        help=f"The verbosity of the logging, max verbosity is -{'v' * (default_log_level // 10 - 1)}",
+    )
+    parser.add_argument(
+        "-y", "--yes",
+        action="store_true",
+        help="Forces yes on all prompts",
+    )
+    return _Config(parser.parse_args(), default_log_level)
+
+
 class _Config:
     __slots__ = ("_namespace", "_default_log_level")
     
@@ -37,37 +71,3 @@ class _Config:
             return val
 
         return self._default_log_level - clamp(self._namespace.v) * 10
-
-
-def setup(parser: ArgumentParser, default_log_level: log.Level, strategies: Iterable[str]) -> Config:
-    assert default_log_level > 9, "Default level below nine will cause unforseen issues"
-    assert (default_log_level / 10).is_integer(), "Default level must be divisable by 10, use the logging constants"
-
-    parser.add_argument(
-        "src",
-        type=Path,
-        help="The source folder to copy from",
-    )
-    parser.add_argument(
-        "dst",
-        type=Path,
-        help="The destination folder to copy to",
-    )
-    parser.add_argument(
-        "-s", "--strategy",
-        choices=list(strategies),
-        help="The strategy to use when doing a backup"
-    )
-    parser.add_argument(
-        "-v",
-        default=0,
-        action="count",
-        help=f"The verbosity of the logging, max verbosity is -{'v' * (default_log_level // 10 - 1)}",
-    )
-    parser.add_argument(
-        "-y", "--yes",
-        action="store_true",
-        help="Forces yes on all prompts",
-    )
-    return Config(parser.parse_args(), default_log_level)
-
